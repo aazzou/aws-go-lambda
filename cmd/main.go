9705d8a529bdceb5cc9810943258aaeb17186f44
Said Aazzou <said.aazzou@cashplus.ma>
2022-11-01 15:29:05 +0100

    Using dynamodb service

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/valyala/fastjson"
 )
 
@@ -20,24 +24,55 @@ type ApiResponse struct {
 	Message string `json:"message"`
 }
 
+func getResponse(status bool, message string) string {
+	msg := fmt.Sprintf(message)
+	resp := ApiResponse{status, msg}
+	content, _ := json.Marshal(resp)
+	return string(content)
+}
+
+var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion(os.Getenv("AWS_REGION")))
+
 func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	req := ApiRequest{}
 	err := fastjson.Validate(request.Body)
 
 	if err != nil {
-		log.Println("Invalid JSON Format")
-		return events.APIGatewayProxyResponse{Body: "Invalid JSON format", StatusCode: 500}, nil
+		content := getResponse(false, "JSON Validation error")
+		return events.APIGatewayProxyResponse{Body: content, StatusCode: 500}, nil
 	}
 
 	body := request.Body
 
 	json.Unmarshal([]byte(body), &req)
-	message := fmt.Sprintf("Welcome %s %s", req.Fname, req.Lname)
 
-	resp := ApiResponse{true, message}
-	content, _ := json.Marshal(resp)
+	session := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-	return events.APIGatewayProxyResponse{Body: string(content), StatusCode: 200}, nil
+	svc := dynamodb.New(session)
+
+	result, err := dynamodbattribute.MarshalMap(req)
+
+	if err != nil {
+		content := getResponse(false, "Parsing JSON error")
+		return events.APIGatewayProxyResponse{Body: content, StatusCode: 500}, nil
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      result,
+		TableName: aws.String(os.Getenv("TABLE_NAME")),
+	}
+
+	_, err = svc.PutItem(input)
+
+	if err != nil {
+		content := getResponse(false, "Writing to database error")
+		return events.APIGatewayProxyResponse{Body: content, StatusCode: 500}, nil
+	}
+
+	content := getResponse(true, fmt.Sprintf("Welcome %s %s", req.Fname, req.Lname))
+	return events.APIGatewayProxyResponse{Body: content, StatusCode: 200}, nil
 }
 
 func main() {
